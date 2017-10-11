d2a6d2e68118473f43ccb6b6c8fb00941f3fd384
Dennis Jung <inylove82@gmail.com>
2017-10-11 13:18:54 +0900

    Add manager methods

--- connswitch/src/main/java/io/djkooks/connswitch/SwitchManager.java
+++ connswitch/src/main/java/io/djkooks/connswitch/SwitchManager.java
@@ -1,11 +1,32 @@
 package io.djkooks.connswitch;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 /**
  * Created by kwangin on 2017/10/10.
  */
 
 public class SwitchManager {
 
-    public SwitchManager () {
+    private static SwitchManager sInstance;
+    private SharedPreferences sp;
+
+    private SwitchManager(Context context) {
+        sp = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static SwitchManager getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new SwitchManager(context);
+        }
+
+        return sInstance;
+    }
+
+    public boolean addSwitch(String switchKey, String url) {
+        sp.edit().putString(switchKey, url).apply();
+        return true;
     }
 }
